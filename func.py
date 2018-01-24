8c4c0a2377fa50b1486eb7338826d99c5b17130b
ZGB <1358794503@qq.com>
2018-01-24 23:25:45 +0800

    invoking function

---
+++ func.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+'''
+def myfunction(num1,num2):
+    ans=num1+num2
+    return ans
+
+myans=myfunction(10,20)
+print("result is:",myans)
+'''
+
+def myfunction():
+    a=10
+    b=20
+    c=a+b
+    print("result is:",c)
+
+myfunction()
